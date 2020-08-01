c48890878ac24cfb6833e353a73c2878f60bbf7d
Joshua D <joshuadelapenha@yahoo.com>
2020-08-01 23:47:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
-  name_hash.each do |key, value|
+  hash.each do |key, value|
     if value < smallest_value || lowest_value == nil
       lowest_key = key
       lowest_value = value
